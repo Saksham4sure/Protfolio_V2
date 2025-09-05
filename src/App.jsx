b9498be6ba038d8e922df0e8576e1f72a0b6cf22
Saksham4sure <sakshamorig123@gmail.com>
2025-09-05 16:21:35 +0545

    1st check

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './Sections/Hero'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <div className='overflow-x-hidden'>
       <Navbar />
       <Hero />
-      <section className='h-screen' id='skills'></section>
     </div>
     </>
   )
